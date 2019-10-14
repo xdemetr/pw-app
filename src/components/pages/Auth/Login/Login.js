f10d42d26368b41f30ff79f67876b83ec403978f
xdemetr <dmitriev@joomdesign.ru>
2019-10-14 15:23:00 +0300

    Lazy-loading.

--- src/components/pages/Auth/Login/Login.js
+++ src/components/pages/Auth/Login/Login.js
@@ -5,6 +5,7 @@ import {login} from '../../../../store/reducers/auth-reducer';
 import {compose} from 'redux';
 import {Redirect} from 'react-router-dom';
 import {getAuth} from '../../../../store/selectors';
+import Spinner from '../../../Spinner';
 
 const Login = (props) => {
 
